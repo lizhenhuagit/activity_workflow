752eae8e5ec7a8577b0733c500fcea8f50331f7d
lizhenhua@beyondcent.com <6639020h>
2020-08-20 15:22:32 +0800

    Initial commit

---
+++ src/main/java/com/imooc/activity_workflow/ActivityWorkflowApplication.java
@@ -0,0 +1,13 @@
+package com.imooc.activity_workflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivityWorkflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivityWorkflowApplication.class, args);
+    }
+
+}
