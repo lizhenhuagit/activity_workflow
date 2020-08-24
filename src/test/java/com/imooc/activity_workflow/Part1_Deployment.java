2aeb4b2deb647348f2f1dfa498ff814fdca4cccc
lizhenhua@beyondcent.com <6639020h>
2020-08-24 20:17:58 +0800

    框架搭建

--- src/test/java/com/imooc/activity_workflow/Part1_Deployment.java
+++ src/test/java/com/imooc/activity_workflow/Part1_Deployment.java
@@ -2,9 +2,14 @@ package com.imooc.activity_workflow;
 
 import org.activiti.engine.RepositoryService;
 import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.repository.DeploymentQuery;
+import org.activiti.engine.repository.ProcessDefinition;
+import org.activiti.engine.repository.ProcessDefinitionQuery;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 
 public class Part1_Deployment extends ActivityWorkflowApplicationTests {
@@ -23,5 +28,15 @@ public class Part1_Deployment extends ActivityWorkflowApplicationTests {
                 .name("流程部署测试候选人task")
                 .deploy();
         System.out.println(deployment.getName());
+    }
+
+    @Test
+    public void getDeploymentBPMN(){
+        List<ProcessDefinition> list = repositoryService.createProcessDefinitionQuery().list();
+        for (ProcessDefinition processDefinition: list) {
+            System.out.println(processDefinition.getVersion());
+            System.out.println(processDefinition.getName());
+            System.out.println(processDefinition.getId());
+        }
     }
 }
